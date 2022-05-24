22777fc292a301f8e60101aa035e7484fc98b303
Евгения Барсукова <e.nevtrinosova@corp.mail.ru>
2022-05-24 08:27:20 +0300

    921 leetcode

---
+++ leetcode/921.ts
@@ -0,0 +1,26 @@
+import {Stack} from '../stack/index';
+
+const minAddToMakeValid = function(s) {
+    const stack = new Stack();
+    s.split('').forEach(symbol => stack.push(symbol));
+
+    let rightsAmount = 0;
+    let leftsAmount = 0;
+    while (!stack.isEmpty()) {
+        let currentSymbol = stack.pop();
+
+        if (currentSymbol === ')') {
+            rightsAmount += 1;
+        }
+
+        if (currentSymbol === '(') {
+            if (rightsAmount > 0) {
+                rightsAmount -= 1;
+            } else {
+                leftsAmount += 1;
+            }
+        }
+    }
+
+    return rightsAmount + leftsAmount;
+};
